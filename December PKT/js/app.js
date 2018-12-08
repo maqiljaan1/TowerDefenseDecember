2425b63cccbd0533babeabf3721f73e85a0001de
Marvin M. Rosales <maqiljaanrosales1@gmail.com>
2018-12-08 19:59:56 +0800

    Add files via upload

--- December PKT/js/app.js
+++ December PKT/js/app.js
@@ -1,6 +1,10 @@
+document.body.style.padding = '0px';
+document.body.style.margin = '0px';
 const can = document.querySelector('canvas');
-can.width = 1580;
-can.height = 600;
+can.width = window.innerWidth * 0.999;
+can.height = window.innerHeight * 0.9920;
+can.style.margin = '0px';
+
 can.style.border = '1px solid black';
 
 const bbox = can.getBoundingClientRect();
@@ -15,15 +19,15 @@ class Mover{
         this.rad = 10;
         this.tx = 0;
         this.ty = 0;
-        this.speed = 0.5;
-        this.originalSpeed = 1; // after moving from one point to another with acceleration speed starts to accumulate
+        this.speed = 5;
+        this.originalSpeed = this.speed; // after moving from one point to another with acceleration speed starts to accumulate
         this.slope = 0;
         this.sx = 0;
         this.sy = 0;
         this.distance = 0;
         this.accel = 0.05;
         this.destinationReached = false;
-        this.maxSpeed = 1;
+        this.maxSpeed = 15;
     }
     draw(){
         c.beginPath();
@@ -102,14 +106,20 @@ class Mover{
     }
 }
 
+
+// this is a hexagon tile
 class Tile{
-    constructor(x,y){
+    constructor(x,y,rad,id){
         this.x = x;
         this.y = y;
-        this.rad = 20;
+        this.rad = rad;
+		this.id = id;
     }
     draw(){
+		c.fillStyle = `rgba(${36},${36},${255},${1})`;
+		c.strokeStyle = `rgba(${255},${255},${25},${1})`;
         c.beginPath();
+		
         c.moveTo(this.x, this.y + this.rad);
         c.lineTo(this.x - Math.cos(Math.PI/6)*this.rad, this.y + Math.sin(Math.PI/6)*this.rad);
         c.lineTo(this.x - Math.cos(Math.PI/6)*this.rad, this.y - Math.sin(Math.PI/6)*this.rad);
@@ -117,8 +127,16 @@ class Tile{
         c.lineTo(this.x + Math.cos(Math.PI/6)*this.rad, this.y - Math.sin(Math.PI/6)*this.rad);
         c.lineTo(this.x + Math.cos(Math.PI/6)*this.rad, this.y + Math.sin(Math.PI/6)*this.rad);
         c.lineTo(this.x, this.y + this.rad);
+		c.fill();
         c.stroke();
         c.closePath();
+		
+		c.beginPath();
+		c.arc(this.x,this.y,this.rad*Math.cos(Math.PI/6),0,2*Math.PI);
+		c.stroke();
+		c.closePath();
+		
+		c.fillText(this.id,this.x,this.y);
     }
     update(){
         this.draw();
@@ -131,21 +149,34 @@ let wraf = null;
 // this is where I click
 let target = {x:can.width/2,y:can.height/2};
 
-let x = new Mover(150,150);
 let y = new Mover(250,250);
 
 let z = [];
 
-for(i = 0; i < 100; i++){
-    let z1 = new Tile(35*i,50);
-    z.push(z1);
+// this is one bad naming convention
+const hexRad = 30
+
+c.textAlign = 'center';
+
+for(j = 0; j < 15; j++){
+	let offset = 0;
+	if(j%2==0){
+		offset = 1;
+	}else{
+		offset = 0;
+	}
+	
+	for(i = 0; i < 26; i++){
+		let z1 = new Tile(offset*(Math.cos(Math.PI/6)*hexRad)+(2*i+1)*(Math.cos(Math.PI/6)*hexRad),2*hexRad*j-j*Math.sin(Math.PI/6)*hexRad,hexRad,`${j}:${i}`);
+		z.push(z1);
+	}
 }
 
 let mainLoop = ()=>{
     c.clearRect(0,0,can.width,can.height);
-    x.update();
+	z.forEach((e,i)=>{e.update();});
     y.update();
-    z.forEach((e)=>e.update());
+    
     wraf = window.requestAnimationFrame(mainLoop);
 };
 
